8dfbfcd614df37005cf9d3909a69392cfc75d0b0
Srushti Tarnalle <124126060+Srushti1722@users.noreply.github.com>
2025-08-21 18:44:28 +0530

    Update ocr.py

--- allerlens-backend/ocr.py
+++ allerlens-backend/ocr.py
@@ -3,7 +3,7 @@ from PIL import Image
 import io
 
 def extract_text_from_image(image_file):
-<<<<<<< HEAD
+
     """
     image_file: either Flask file (has .stream) or BytesIO object
     Works for both single image upload and live scan frames.
@@ -17,7 +17,7 @@ def extract_text_from_image(image_file):
     except Exception as e:
         print("OCR failed:", e)
         return ""
-=======
+
     image = Image.open(image_file.stream)
     return pytesseract.image_to_string(image)
->>>>>>> c396e64d591b9449b46e280c6dcf9520592c4ccc
+
